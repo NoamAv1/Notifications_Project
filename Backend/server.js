feca23d934f75d62f0567027c5017938efacda5e
noam <noam@bwitty.com>
2021-09-04 20:41:07 +0300

    add server.js

---
+++ Backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+
+app.get("/ping", (req, res) => {
+    res.json({ message: "pong" });
+});
+  
